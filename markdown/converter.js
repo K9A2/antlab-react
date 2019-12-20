132a05aaad986a265574d0397a1a2594438a3dd2
stormlin <lin-jinting@outlook.com>
2019-12-20 05:52:52 +0000

    解决 loadMarkdownFile 函数在解析字符串上的 bug

--- markdown/converter.js
+++ markdown/converter.js
@@ -1,6 +1,8 @@
 const yaml = require('yaml-front-matter');
 const fs = require('fs');
 
+const monthString = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jnu', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
 /**
  * 获取 rootPath 下的所有子目录名字
  *
@@ -17,6 +19,12 @@ exports.getTheNameOfAllChildFolders = function(rootPath) {
   return folders;
 };
 
+/**
+ * 获取 parentDirPath 下所有的文件名
+ *
+ * @param {string} parentDirPath 目标文件夹路径
+ * @return 该文件夹的所有文件的文件名数组
+ */
 exports.getTheNameOfAllFiles = function(parentDirPath) {
   const fileNames = [];
   const parentDir = fs.readdirSync(parentDirPath);
@@ -31,18 +39,25 @@ exports.getTheNameOfAllFiles = function(parentDirPath) {
 /**
  * 以  UTF-8 方式读取带 YAML 头的 Markdown 源文件
  *
- * @param filePath Markdown 源文件路径
- * @returns {{}} 解析 YAML 头之后的 Markdown 源文件
+ * @param {string} filePath Markdown 源文件路径
+ * @returns {object} 解析 YAML 头之后的 Markdown 源文件
  */
 exports.loadMarkdownFile = function(filePath) {
   // YAML 头信息会放在返回值的 '_content' 字段中
   const yamlLoadedFile = yaml.loadFront(fs.readFileSync(filePath, 'utf-8'));
 
   // parse the date into formatted time string
-  const postDate = new Date(yamlLoadedFile.date);
-  const year = postDate.getFullYear();
-  const month = postDate.getMonth() < 10 ? `0${postDate.getMonth()}` : postDate.getMonth();
-  const day = postDate.getDay() < 10 ? `0${postDate.getDay()}` : postDate.getDay();
+  const postDate = `${yamlLoadedFile.date}`;
+  const dateParts = postDate.split(' ');
+  const year = `${dateParts[3]}`;
+  let month = monthString.indexOf(dateParts[1]) + 1;
+  const day = `${dateParts[2]}`;
+
+  if (month < 10) {
+    month = `0${month}`;
+  } else {
+    month = `${month}`;
+  }
 
   // remove the first line break character ('\n')
   let markdownContent = yamlLoadedFile.__content;
@@ -53,5 +68,100 @@ exports.loadMarkdownFile = function(filePath) {
     top: yamlLoadedFile.top,
     date: `${year}-${month}-${day}`,
     content: markdownContent,
+  };
+};
+
+/**
+ * 从文件名列表中过滤出指定扩展名的文件
+ *
+ * @param {object} fileNameList 文件名列表
+ * @param extName 目标文件的扩展名
+ * @return 只包含指定扩展名的文件名列表
+ */
+exports.filterOutFileNamesByExtName = function(fileNameList, extName) {
+  const markdownFileNames = [];
+  fileNameList.forEach(function(fileName) {
+    const lastIndexOfDot = fileName.lastIndexOf('.');
+    if (lastIndexOfDot === -1) {
+      // 该文件没有扩展名，不是 Markdown 源文件
+      return;
+    }
+    const ext = fileName.substring(lastIndexOfDot, fileName.length);
+    if (ext !== extName) {
+      // 该文件的扩展名不是 '.md'，不是 Markdown 源文件
+      return;
+    }
+    markdownFileNames.push(fileName);
+  });
+  return markdownFileNames;
+};
+
+/**
+ * 按照对象的日期属性进行排序，排序功能依赖于待比较对象的 'date' 属性。
+ * 结果按照从现在到过去的顺序排列。
+ *
+ * @param {object} firstElement 第一个待比较的元素
+ * @param {object} secondElement 第二个待比较的元素
+ * @return {number} 比较结果
+ */
+exports.sortByDate = function(firstElement, secondElement) {
+  const dateOne = new Date(firstElement.date);
+  const dateTwo = new Date(secondElement.date);
+  return -(dateOne - dateTwo);
+};
+
+/**
+ * 加载指定目录下的所有 Markdown 源文件、解析文件并排序
+ *
+ * @param filePath 存放 Markdown 文件的文件夹路径
+ * @return {object} 返回一个包含该文件夹下所有解析过后的 Markdown 文件的列表，
+ *                  先按照是否置顶排序，然后按照日期先后排序
+ */
+exports.buildFileObjectList = function(filePath) {
+  const topPosts = []; // 置顶的文章
+  const nonTopPosts = []; // 非置顶的文章
+
+  // 读取所有 Markdown 源文件的文件名
+  const markdownFileNames = module.exports.filterOutFileNamesByExtName(
+    module.exports.getTheNameOfAllFiles(filePath), '.md'
+  );
+
+  // 读取并解析 Markdown 文件对象
+  const markdownFileList = [];
+  markdownFileNames.forEach(function(fileName) {
+    markdownFileList.push(
+      module.exports.loadMarkdownFile(filePath + fileName)
+    );
+  });
+
+  // 先按照是否指定分类，然后在两个分类之间按照发表时间排序
+  markdownFileList.forEach(function(fileItem) {
+    if (fileItem.top === true) {
+      topPosts.push(fileItem);
+      return;
+    }
+    nonTopPosts.push(fileItem);
+  });
+  topPosts.sort(module.exports.sortByDate);
+  nonTopPosts.sort(module.exports.sortByDate);
+
+  const result = [];
+  result.push(...topPosts);
+  result.push(...nonTopPosts);
+  return result;
+};
+
+/**
+ * 解析形如 2019-01-01 这样的日期字符串，并返回日期对象
+ *
+ * @param {string} timeString 待解析的日期字符串
+ * @return {object} 包含年月日（year，month 和 day）的对象
+ */
+exports.parseTimeStringIntoDateObject = function(timeString) {
+  const params = timeString.split('-');
+  return {
+    year: params[0],
+    month: params[1],
+    day: params[2],
   };
 };
--- test/converter.test.js
+++ test/converter.test.js
@@ -1,33 +1,101 @@
 const assert = require('assert');
 const converter = require('../markdown/converter');
 
-describe('it should convert markdown file to html file', function() {
-  it('should return the names of folders under markdown', function() {
+describe('it should convert markdown file to html file', function () {
+  it('should return the names of folders under markdown', function () {
     const expected = ['activities', 'announcements', 'introductions', 'members'];
     const actual = converter.getTheNameOfAllChildFolders('./markdown/');
     assert.deepStrictEqual(actual, expected);
   });
 
-  it('should return the names of folders under project root', function() {
+  it('should return the names of folders under project root', function () {
     const expected = ['.git', 'config', 'markdown', 'node_modules', 'src', 'test'];
     const actual = converter.getTheNameOfAllChildFolders('./');
     assert.deepStrictEqual(actual, expected);
   });
 
-  it('should return the file names', function() {
+  it('should return the file names', function () {
     const expected = ['README.txt', 'converter.js'];
     const actual = converter.getTheNameOfAllFiles('./markdown/');
     assert.deepStrictEqual(actual, expected);
   });
 
-  it('should return the parsed yaml part of markdown file', function() {
+  it('should return the parsed yaml part of markdown file', function () {
     const expected = {
       title: '2019年9月迎新聚餐',
-      date: '2019-09-04',
+      date: '2019-10-10',
       top: false,
-      content: '2019年9月，实验室迎来新成员，分别是梁文杰、叶子豪、郑雨昕，欢迎三位新同学加入实验室的大家庭。\n',
+      content: '2019年9月，实验室迎来新成员，分别是梁文杰、叶子豪、郑雨昕，欢迎三位新同学加入实验室的大家庭。\n\n' +
+        '![实验室聚餐合照](./files/a0131d42-4c6c-477f-8669-17125fbc0817.jpg)\n',
     };
     const actual = converter.loadMarkdownFile('./markdown/activities/2019年9月迎新聚餐.md');
     assert.deepStrictEqual(actual, expected);
+  });
+
+  it('should only list all file names of markdown files', function () {
+    const expected = [
+      '2017年12月29日实验室部分研究生游清远.md',
+      '2019年6月实验室成员游番禺宝墨园、佛山顺德.md',
+      '2019年9月迎新聚餐.md',
+      '欢送实验室2016级专硕研究生张羽翔和张凤洁顺利毕业.md',
+      '欢送实验室2016级学硕研究生张媛顺利毕业.md',
+    ];
+    const actual = converter.filterOutFileNamesByExtName(
+      converter.getTheNameOfAllFiles('./markdown/activities/'), '.md'
+    );
+    assert.deepStrictEqual(actual, expected);
+  });
+
+  it('should print all markdown file object', function () {
+    const markdownFileList = [
+      '实验室硕士生林晋霆的论文被国际期刊Computer Networks录用.md',
+      '实验室硕士生张羽翔的论文被国际期刊IEEE Transactions on Network and Service Management录用.md',
+      '实验室研究生荣获研究生2019年度国家研究生奖学金.md',
+      '实验室研究生荣获研究生2020年度国家研究生奖学金.md',
+      '实验室研究生荣获研究生2020年度国家研究生奖学金.md',
+    ];
+    const expect = [
+      {
+        content: '\n' +
+          '实验室长期招收硕士研究生(科学及专业学位)和有研究兴趣的本科生：\n' +
+          '\n' +
+          '如果您对计算机网络，特别是云计算、数据中心网络、软件定义网络（SDN）、网络虚拟化、网络拥塞控制等方向的学术研究和系统应用等感兴趣，希望攻读硕士学位，并具有一定的编程能力和英文阅读写作能力，欢迎随时和我们联系！欢迎推免研究生联系！\n' +
+          '\n' +
+          '我们与国外研究小组有长期联系，优秀学生可以推荐出国攻读博士学位（如英国等地高校）。\n' +
+          '\n' +
+          '具体信息请见：https://antlab.jnu.edu.cn/cuilin/join\n' +
+          '\n' +
+          '联系方式：tcuilin@jnu.edu.cn / cuilincui@gmail.com\n',
+        date: '2019-08-12',
+        title: '【招生】实验室长期招收硕士研究生和有研究兴趣的本科生',
+        top: true,
+      },
+      {
+        content: '\n实验室研究生林晋霆同学，荣获2020年度国家研究生奖学金。林晋霆同学读研期间，在COMNET（Computer Network，CCF B类）期刊上发表论文《Extensive Evaluation on the Performance and Behaviour of TCP Congestion Control Protocols under Varied Network Scenarios》。\n',
+        date: '2019-12-12',
+        title: '实验室研究生荣获研究生2020年度国家研究生奖学金',
+        top: false,
+      },
+      {
+        content: '\n实验室硕士生张羽翔等人撰写的论文《Mystique: A Fine-grained and Transparent Congestion Control Enforcement Scheme》被IEEE出版社的国际学术期刊《IEEE Transactions on Network and Service Management》录用。论文将于2020年正式发表。\n',
+        date: '2019-09-09',
+        title: '实验室硕士生张羽翔的论文被国际期刊IEEE Transactions on Network and Service Management录用',
+        top: false,
+      },
+      {
+        content: '\n实验室硕士生林晋霆的论文被国际期刊Computer Networks录用\n',
+        date: '2019-09-08',
+        title: '实验室硕士生林晋霆的论文被国际期刊Computer Networks录用',
+        top: false,
+      },
+      {
+        content: '\n实验室研究生张媛，因在2017~2018学年发表1篇期刊论文和2篇会议论文，荣获2019年研究生国家奖学金。\n',
+        date: '2019-08-12',
+        title: '实验室研究生荣获研究生2019年度国家研究生奖学金',
+        top: false,
+      },
+    ];
+    const actual = converter.buildFileObjectList('./markdown/announcements/');
+    assert.deepStrictEqual(actual, expect);
   });
 });
